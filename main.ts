8ea92a2e1116666e711ef75b7f30c5658d560702
louis-chapuis-sonarsource <115462438+louis-chapuis-sonarsource@users.noreply.github.com>
2023-04-24 14:22:52 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,5 @@ const exec = require('@actions/exec');
 
 let errorMessage = '';
 const options = {};
-options.listeners = {
-  stderr: (data: Buffer) => {
-    errorMessage += data.toString()
-  }
-}
 
 await exec.exec("sonar-scanner", ["-Dsonar.cfamily.build-wrapper-output=bw-output", "-Dsonar.projectKey=louis-chapuis-sonarsource_conan-mac-os_AYct2Xs62boBvgt9JBvR"], options)
